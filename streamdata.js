ccd030a438c894123265e7bef0bea547c1acb4bd
Suyash Gupta <gsuyash9@gmail.com>
2020-12-26 02:25:57 +0530

    first stream and buffer code

--- streamdata.js
+++ streamdata.js
@@ -1,9 +1,11 @@
 var http=require('http');
 var fs=require('fs');
 
-var myStream = fs.createReadStream(__dirname + '/readData.txt','utf8')
+var myStream = fs.createReadStream(__dirname + '/readData.txt','utf8');
+var mywriteStream = fs.createWriteStream(__dirname + '/writeData.txt');
 
 myStream.on('data',(chunk)=>{
     console.log('new buffer added');
     console.log(chunk);
+    mywriteStream.write(chunk);
 })
